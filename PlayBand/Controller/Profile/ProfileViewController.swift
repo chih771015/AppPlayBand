c0c98705b0d3b2fccd3a426bdcb49d70652b0651
姜旦旦 <chiangchihhsuan@jiangdandans-MacBook-Pro.local>
2019-04-03 16:13:01 +0800

    Profile Controller setting navigation bar.

---
+++ PlayBand/Controller/Profile/ProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewController.swift
+//  PlayBand
+//
+//  Created by 姜旦旦 on 2019/4/3.
+//  Copyright © 2019 姜旦旦. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
