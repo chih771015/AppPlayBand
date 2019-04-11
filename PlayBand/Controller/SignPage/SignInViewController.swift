7748a68347177e558c0f093c1720409f54f372e0
姜旦旦 <chiangchihhsuan@jiangdandans-MacBook-Pro.local>
2019-04-11 11:56:26 +0800

    SignIN ViewController Setup.

---
+++ PlayBand/Controller/SignPage/SignInViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SignInViewController.swift
+//  PlayBand
+//
+//  Created by 姜旦旦 on 2019/4/11.
+//  Copyright © 2019 姜旦旦. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    @IBOutlet weak var signInButton: UIButton!
+    
+    @IBOutlet weak var guestButton: UIButton!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var changeModelButton: UIButton!
+    
+    @IBAction func changeModelAction() {
+    }
+    @IBOutlet weak var accountTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
